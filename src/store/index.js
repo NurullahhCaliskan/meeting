0159fb8a42f857433c4722b5f10aba8c872113a3
ncaliskan@ipera.com.tr <Vvnp9j27a>
2021-03-15 21:57:13 +0300

    feature: add validation and vuex

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,23 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        meetingTypeList: [
+            {id: 1, text: 'Very Important', color: '#FF0000'},
+            {id: 2, text: 'Emergency', color: '#FFA500'},
+            {id: 3, text: 'Normal', color: '#800080'},
+            {id: 4, text: 'Not Important', color: '#808080'},
+            {id: 5, text: 'Meeting', color: '#0000FF'}
+        ]
+    },
+    mutations: {
+        addMeetingType(state, data) {
+            state.meetingTypeList.push(data);
+        },
+        deleteMeetingType(state, data) {
+            state.meetingTypeList = data;
+        },
+    },
+    actions: {},
+    modules: {}
 })
--- src/views/MeetingCalendar/SelectMeetingType.vue
+++ src/views/MeetingCalendar/SelectMeetingType.vue
@@ -4,13 +4,15 @@
       <v-card-title>
         <span class="headline">Edit Meeting Type</span>
       </v-card-title>
-      <v-card-text>
+      <v-card-text :class="{ 'form-group--error': $v.name.$error }">
         <v-container>
           <v-row>
             <v-col cols="12">
-              <v-text-field label="Meeting Type Name" required hint="For example: Important, Emergency ...">
+              <v-text-field v-model.trim="$v.name.$model" label="Meeting Type Name" required hint="For example: Important, Emergency ...">
 
               </v-text-field>
+              <v-chip class="error" v-if="addNewMeetingType && !$v.name.required">Field is required</v-chip>
+              <v-chip class="error" v-if="addNewMeetingType && !$v.name.minLength">Name must have at least {{ $v.name.$params.minLength.min }} letters.</v-chip>
             </v-col>
             <v-col cols="12">
               <label> Select meeting datetime*</label>
@@ -21,17 +23,17 @@
               <small>*indicates required field</small>
             </v-col>
             <v-col cols="12">
-              <v-btn class="align-content-lg-center" text color="red">
+              <v-btn class="align-content-lg-center" text color="red" @click="save">
                 Add
               </v-btn>
             </v-col>
+            <v-chip class="error" v-for="(item,index) in addMeetingTypeStatus" :key="index">{{ item }}</v-chip>
           </v-row>
         </v-container>
-
         <!-- List meeting type-->
         <v-card>
           <v-list class="pt-0" flat>
-            <div v-for="(item) in meetingTypeList" :key="item.id">
+            <div v-for="(item) in $store.state.meetingTypeList" :key="item.id">
 
               <v-list-item>
                 <v-list>
@@ -57,10 +59,10 @@
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="blue darken-1" text @click="dialog = false">
+        <v-btn color="blue darken-1" text @click="close">
           Close
         </v-btn>
-        <v-btn color="blue darken-1" text @click="dialog = false">
+        <v-btn color="blue darken-1" text @click="close">
           Save
         </v-btn>
       </v-card-actions>
@@ -69,23 +71,51 @@
 </template>
 
 <script>
+import {required, minLength, between} from 'vuelidate/lib/validators'
+
 export default {
+
   data: () => ({
+    name: '',
+    addMeetingTypeStatus: [],
+    addNewMeetingType: false,
     dialog: false,
-    value1: '',
-    color: '',
-    meetingTypeList: [
-      {id: 1, text: 'Very Important', color: '#FF0000'},
-      {id: 2, text: 'Emergency', color: '#FFA500'},
-      {id: 3, text: 'Normal', color: '#800080'},
-      {id: 4, text: 'Not Important', color: '#808080'},
-      {id: 5, text: 'Meeting', color: '#0000FF'}
-    ]
+    color: ''
   }),
 
   methods: {
     deleteMeet(id) {
-      this.meetingTypeList = this.meetingTypeList.filter(meet => meet.id !== id);
+      this.$store.commit('deleteMeetingType', this.$store.state.meetingTypeList.filter(meet => meet.id !== id))
+    },
+
+    save() {
+      this.addMeetingTypeStatus = [];
+      this.addNewMeetingType = true;
+      this.$v.$touch();
+
+      this.$store.state.meetingTypeList.find(item => item.text === this.name) !== undefined ? this.addMeetingTypeStatus.push('Name must be unique') : undefined;
+      this.$store.state.meetingTypeList.find(item => item.color === this.color) !== undefined ? this.addMeetingTypeStatus.push('Color must be unique') : undefined;
+
+
+      if (this.addMeetingTypeStatus.length === 0 && !this.$v.$invalid) {
+        this.$store.commit('addMeetingType', {id: Date.now(), text: this.name, color: this.color});
+      }
+    },
+    close() {
+      this.refresh();
+    },
+    refresh() {
+      this.name = '';
+      this.addMeetingTypeStatus = [];
+      this.addNewMeetingType = false;
+      this.dialog = false;
+      this.color = '';
+    }
+  },
+  validations: {
+    name: {
+      required,
+      minLength: minLength(4)
     },
   }
 }
